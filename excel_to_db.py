c5a218981933390a6a520e4f46e1a54425427d8e
gaetan.maronne <gaetan.maronne@epita.fr>
2025-09-17 12:52:28 +0200

    fix: python removed useless col

--- excel_to_db.py
+++ excel_to_db.py
@@ -7,25 +7,41 @@ def xlsx_to_df(file_path):
     """Reads an Excel file and returns a pandas DataFrame."""
     df = pd.read_excel(file_path)
     return df
+def clean_df(df):
+    """Cleans the DataFrame by removing columns with any missing values."""
+    df_cleaned = df.drop('Unnamed: 1', axis=1, inplace=True, errors='ignore')
+    df_cleaned = df.drop('Unnamed: 3', axis=1, inplace=True, errors='ignore')
+    df_cleaned = df.drop('Unnamed: 5', axis=1, inplace=True, errors='ignore')
+    df_cleaned = df.drop('Unnamed: 6', axis=1, inplace=True, errors='ignore')
+    df_cleaned = df.drop('Unnamed: 8', axis=1, inplace=True, errors='ignore')
+    df_cleaned = df.drop('Unnamed: 10', axis=1, inplace=True, errors='ignore')
+    df_cleaned = df.drop('Unnamed: 12', axis=1, inplace=True, errors='ignore')
+    df_cleaned = df.drop('Unnamed: 14', axis=1, inplace=True, errors='ignore')
+    df_cleaned = df.drop('Unnamed: 16', axis=1, inplace=True, errors='ignore')
+    df_cleaned = df.drop('Unnamed: 18', axis=1, inplace=True, errors='ignore')
+    df_cleaned = df.drop('Unnamed: 20', axis=1, inplace=True, errors='ignore')
+    df_cleaned = df.drop('Unnamed: 22', axis=1, inplace=True, errors='ignore')
+    df_cleaned = df.drop('Unnamed: 24', axis=1, inplace=True, errors='ignore')
+    df_cleaned = df.drop('Unnamed: 26', axis=1, inplace=True, errors='ignore')
+    df_cleaned = df.drop('Unnamed: 28', axis=1, inplace=True, errors='ignore')
+    df_cleaned = df.drop('Unnamed: 30', axis=1, inplace=True, errors='ignore')
+    df_cleaned = df.drop('Unnamed: 32', axis=1, inplace=True, errors='ignore')
+    df_cleaned = df.drop('Unnamed: 34', axis=1, inplace=True, errors='ignore')
+    df_cleaned = df.drop('Unnamed: 38', axis=1, inplace=True, errors='ignore')
+    df_cleaned = df.drop('Unnamed: 40', axis=1, inplace=True, errors='ignore')
+    df_cleaned = df.drop('Unnamed: 42', axis=1, inplace=True, errors='ignore')
+    df_cleaned = df.drop('Unnamed: 44', axis=1, inplace=True, errors='ignore')
 
-def plot_df(df, x_col, y_col):
-    """Plots the specified columns of the DataFrame."""
-    plt.figure(figsize=(10, 6))
-    plt.plot(df[x_col], df[y_col], marker='o')
-    plt.title(f'{y_col} vs {x_col}')
-    plt.xlabel(x_col)
-    plt.ylabel(y_col)
-    plt.grid(True)
-    plt.show()
+    return df_cleaned
 
 def main():
     print("Starting the Excel to DataFrame conversion...")
     file_path = 'ressource/matrice.xlsx'  # Path to your Excel file
     df = xlsx_to_df(file_path)
-    
+    clean_df(df)
     # Display the first few rows of the DataFrame
     print(df.head(15))
     with open('output.txt', 'w') as f:
-        f.write(df.head().to_string())
+        f.write(df.head(15).to_string())
 
 main()
