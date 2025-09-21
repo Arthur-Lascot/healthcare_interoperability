6585c9d57d58150e3256c61b0eb1ce75fc795273
arthur.oldrati <arthur.oldrati@epita.fr>
2025-09-21 10:46:26 +0200

    feat: Now listen on port 3002

--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
   console.log(`✅ Serveur sur http://localhost:${PORT}`);
