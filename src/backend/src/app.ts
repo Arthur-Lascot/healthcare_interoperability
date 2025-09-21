b9150987702386fbfccd7de5c969cf12617e7069
arthur.oldrati <arthur.oldrati@epita.fr>
2025-09-21 12:39:57 +0200

    feat: docker-compose featuring keycloak serveur along with frontend and backend

--- src/backend/src/app.ts
+++ src/backend/src/app.ts
@@ -1,10 +1,23 @@
 import express from "express";
+import cors from "cors";
 import file_routes from "./routes/files.routes";
 import { authMiddleware } from "./middlewares/auth_handler";
 import { errorHandler } from "./middlewares/error_handler";
 import { NotFoundError } from "./errors/AppError";
 
 const app = express();
+
+const corsOptions = {
+  origin: [
+    'http://localhost:3000',
+    'http://frontend:3000'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
@@ -14,7 +27,7 @@ app.use(authMiddleware);
 
 app.use("/api", file_routes);
 
-app.all('*', (req, res, next) => {
+app.use((req, res, next) => {
   const err = new NotFoundError(`Cannot find ${req.originalUrl} on this server`);
   next(err);
 });
