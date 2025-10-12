ad5a5af17737bff2f69928a1e540daa43364a5d6
arthur.oldrati <arthur.oldrati@epita.fr>
2025-10-12 11:44:23 +0200

    feat: Added DocumentReferenceParams for initialisation as well as validation check when initialising a DocumentReference object

--- src/backend/src/entities/DocumentReference.ts
+++ src/backend/src/entities/DocumentReference.ts
@@ -3,13 +3,15 @@ import Coding from "../models/Coding";
 import Context from "../models/Context";
 import Identifier from "../models/Identifier";
 import Reference from "./Reference";
+import DocumentReferenceParams from "../models/DocumentReferenceParams";
+import { ValidationError } from "../errors/AppError";
 
 class DocumentReference {
-    readonly ressourceType:                  String;
+    readonly ressourceType!:                 String;
     readonly masterIdentifier?:              Identifier;
     readonly identifier?:                    Identifier[];
-    readonly status:                         String;
-    readonly docstatus:                      String;
+    readonly status!:                        String;
+    readonly docstatus?:                     String;
     readonly type?:                          CodeableConcept;
     readonly category?:                      CodeableConcept[];
     readonly subject?:                       Reference;
@@ -20,15 +22,33 @@ class DocumentReference {
     readonly relatesTo?:                     {code: String, target: Reference}[];
     readonly description?:                   String;
     readonly securityLabel?:                 CodeableConcept[];
-    readonly content:                        {attachment: undefined, format?: Coding}[];
+    readonly content!:                       {attachment: undefined, format?: Coding}[];
     readonly context?:                       Context;
 
-    constructor(status: String, docstatus: String, content: {attachment: undefined, format?: Coding}[]) {
-        this.ressourceType = "DocumentReference";
-        this.status = status;
-        this.docstatus = docstatus;
-        this.content = content;
-    }
+    constructor(params: DocumentReferenceParams) {
+        Object.assign(this, params);
+
+        if (this.status !== "current" && this.status !== "superseded" && this.status !== "entered-in-error") {
+            throw new ValidationError(`Status field is set with an incorrect value "${this.status}" 
+                when trying to initialise DocumentReference`);
+        }
+
+        if (this.docstatus) {
+            if (this.docstatus !== "preliminary" && this.docstatus !== "final" && this.docstatus !== "amended"
+                && this.docstatus !== "entered-in-error") {
+                throw new ValidationError(`DocStatus field is set with an incorrect value "${this.docstatus}" 
+                    when trying to initialise DocumentReference`);
+            }
+        }
+
+        if (this.relatesTo) {
+            for (const code in this.relatesTo)
+                if (code !== "replaces" && code !== "transforms" && code !== "signs" && code !== "appends") {
+                    throw new ValidationError(`Code field in relatesTo field is set with an incorrect value "${code}" 
+                        when trying to initialise DocumentReference`);
+                }
+        }
+  }
 }
 
 export default DocumentReference;
