8f401488ef2af3bb9fe938f072aebf979d7a5189
arthur.oldrati <arthur.oldrati@epita.fr>
2025-11-08 15:50:59 +0100

    fix: Tests pipeline

--- db/init/seed.sql
+++ db/init/seed.sql
@@ -8,6 +8,6 @@ CREATE TABLE IF NOT EXISTS documents (
 );
 
 INSERT INTO documents (code, class_code_display_name, loinc, type_code_display_name, content) VALUES
-(1001, 'Classe A', '1234-5', 'Type X', 'contenu test 1'),
+('11111111-1111-1111-1111-111111111111', 1001, 'Classe A', '1234-5', 'Type X', 'contenu test 1'),
 (1002, 'Classe B', '6789-0', 'Type Y', 'contenu test 2')
 ON CONFLICT DO NOTHING;
