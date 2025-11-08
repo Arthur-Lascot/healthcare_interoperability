954143c5998a3805c682f9256decd972df1d8434
arthur.oldrati <arthur.oldrati@epita.fr>
2025-11-08 18:10:59 +0100

    fix: replaced httpYAC with Jest

---
+++ tests/http/getFile.test.js
@@ -0,0 +1,27 @@
+const supertest = require('supertest');
+const { API_BASE, getToken } = require('./helpers');
+
+describe('GET /api/file/:uuid', () => {
+  let token;
+
+  beforeAll(async () => {
+    token = await getToken();
+  });
+
+  test('should return 200 and correct JSON', async () => {
+    const uuid = '11111111-1111-1111-1111-111111111111';
+    const res = await supertest(API_BASE)
+      .get(`/api/file/${uuid}`)
+      .set('Authorization', `Bearer ${token}`);
+    
+    expect(res.status).toBe(200);
+  });
+
+  test('should return 404 for unknown UUID', async () => {
+    const res = await supertest(API_BASE)
+      .get(`/api/file/00000000-0000-0000-0000-000000000000`)
+      .set('Authorization', `Bearer ${token}`);
+    
+    expect(res.status).toBe(404);
+  });
+});
---
+++ tests/http/helper.js
@@ -0,0 +1,15 @@
+const supertest = require('supertest');
+
+const API_BASE = 'http://localhost:8080';
+
+async function getToken() {
+  const res = await supertest(API_BASE)
+    .post('/realms/healthcare/protocol/openid-connect/token')
+    .type('form')
+    .send(`client_id=HealthApp&grant_type=password&username=${TEST_USER_USERNAME}&password=${TEST_USER_PWD}`);
+    
+  if (!res.body.access_token) throw new Error('Token not received');
+  return res.body.access_token;
+}
+
+module.exports = { API_BASE, getToken };
