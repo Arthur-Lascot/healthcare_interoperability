5481ab1b24ac0948125ddd3d652aa07ae78fe92a
arthur.oldrati <arthur.oldrati@epita.fr>
2025-11-10 15:24:43 +0100

    feat: Remodeled repository with new db and converting it to DocumentMOS + remodeled service as well

--- src/backend/src/repositories/file_repository.ts
+++ src/backend/src/repositories/file_repository.ts
@@ -2,6 +2,8 @@ import { UUID } from "crypto";
 import { FileEntity } from "../DTO/FileEntity";
 import { FileNotFoundError } from "../errors/FileNotFound";
 import { Pool } from "pg";
+import DocumentMOS from "../models/DocumentMOS";
+import Code from "../utils/structure/MOS/Code";
 
 const pool = new Pool({
   host: process.env.DB_HOST,
@@ -11,16 +13,44 @@ const pool = new Pool({
   database: process.env.DB_NAME,
 });
 
-export const getDocumentReferenceFromUUID = async (uuid: UUID): Promise<FileEntity> => {
+export const getDocumentReferenceFromUUID = async (uuid: UUID): Promise<DocumentMOS> => {
     const result = await pool.query(
-      "SELECT * FROM documents WHERE uuid = $1;",
+      `SELECT 
+        d.id AS document_id,
+        c.valeur AS code_valeur,
+        c.libelle AS code_libelle,
+        c.langue AS code_langue,
+        c.identifiant_nomenclature AS code_identifiant_nomenclature,
+        c.nom_nomenclature AS code_nom_nomenclature,
+        c.version_nomenclature AS code_version_nomenclature,
+        c.uri_nomenclature AS code_uri_nomenclature,
+        c.identifiant_agence AS code_identifiant_agence,
+        c.nom_agence AS code_nom_agence,
+        m.id AS metadonnee_id,
+        m.author,
+        m.creation_date,
+        m.status,
+        m.location,
+        m.access_logs,
+        m.raw_fhir
+      FROM document_mos d
+      LEFT JOIN mos_code c ON d.type_document_id = c.id
+      LEFT JOIN mos_document_metadonnee m ON d.metadonnee_id = m.id
+      WHERE d.id = $1
+      LIMIT 1;`,
       [uuid]
     );
 
     if (result.rows.length === 0) {
         throw new FileNotFoundError(uuid);
     }
-    return result.rows[0];
+
+    const row = result.rows[0];
+    const code: Code = new Code({code: row.code_valeur, display: row.code_libelle, system: row.code_identifiant_nomenclature, version: row.code_version_nomenclature}, row.code_langue);
+    const metadonnee = {author: row.author, creationDate: row.creation_date, status: row.status, location: row.location, accessLogs: row.access_logs, rawFHIR: row.raw_fhir};
+    const document: DocumentMOS = new DocumentMOS({typeDocument: code, meatdonnee: metadonnee});
+
+    return document;
 };
 
 export const getAllFiles = async(): Promise<FileEntity[]> => {
--- src/backend/src/services/files_services.ts
+++ src/backend/src/services/files_services.ts
@@ -4,27 +4,32 @@ import { FileEntity } from "../DTO/FileEntity"
 import rawHabilitation from "../habilitation_matrix.json"
 import { UUID } from "crypto";
 import { getLogger } from "../logger/loggerContext";
+import DocumentMOS from "../models/DocumentMOS";
 
 
 const habilitation: Record<string, boolean[]> = rawHabilitation;
 
-const isReadableBy = (file: FileEntity, role: Role,): boolean => {
+const isReadableBy = (document: DocumentMOS, role: Role,): boolean => {
 
-    const accessList : boolean[] = habilitation[file.loinc];
+    if (!document.typeDocument) {
+        return true;
+    }
+
+    const accessList : boolean[] = habilitation[document.typeDocument?.valeur];
     return accessList[role];
 }
 
 export const getDocumentReference = async (role: Role, fileUUID: UUID): Promise<boolean> => {    
-    const file: FileEntity = await FileRepository.getDocumentReferenceFromUUID(fileUUID);
-    return isReadableBy(file, role);
+    const document: DocumentMOS = await FileRepository.getDocumentReferenceFromUUID(fileUUID);
+    return isReadableBy(document, role);
 }
 
-export const getAllAccessibleFile = async (role: Role): Promise<FileEntity[]> => {
+/*export const getAllAccessibleFile = async (role: Role): Promise<FileEntity[]> => {
     const files: FileEntity[] = await FileRepository.getAllFiles();
     let accessibleFiles: FileEntity[] = [];
     files.forEach((f: FileEntity) => { if (isReadableBy(f, role)) {accessibleFiles.push(f)} })
     return accessibleFiles;
-}
+}*/
 
 export const createFile = async (file: FileEntity): Promise<number> => {
     return await FileRepository.createFile(file);
